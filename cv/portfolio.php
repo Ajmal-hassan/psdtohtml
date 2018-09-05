33c33aeec3f7dcd09dd56e625dbf17c6d1edf170
ajmal\Ajmal hassan <ajmalhassan651@gmail.com>
2018-09-05 10:41:20 +0500

    CV created by using HTMl & CSS

---
+++ cv/portfolio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ajmal Hasssan</title>
+        <link href="css/global.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="main-container">
+            <img src="images/profile-image.png" title="Ajmal hassan">
+            <h1>Ajmal Hassan</h1>
+            <p>Web-Developer</p>
+            <div class="social">
+                <ul>
+                  
+                    <li><a href="#"><img src="images/facebook.png"></a></li>
+                    <li><a href="#"><img src="images/Google-plus.png"></a></li>
+                    <li><a href="#"><img src="images/linkedin.png"></a></li>
+                    <li><a href="#"><img src="images/twitter.png"></a></li>
+               </ul>
+            </div><br/>
+            <div class="portfolio">
+                <h2>Portfolio</h2>
+                <div class="portfolio-top">
+                    <div class="top-left">
+                        <h3>Responsive Design</h3>  
+                    </div>
+                    <div class="top-right">
+                        <h3>Ecommerce Websites</h3>
+                    </div>
+                </div>
+                <div class="portfolio-bottom">
+                    <div class="bottom-left">
+                        <h3>Website Management</h3>
+                    </div>
+                    <div class="bottom-right">
+                        <h3>Website Design</h3>
+                    </div>
+                    <div class="bottom-right-bottom">
+                        <h3>Portfolio Template Design</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </body>
+</html>
